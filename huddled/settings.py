1d59df0d27992df29635066742b3c57f4f5f6397
Preetam Raj <iitianpreetam@gmail.com>
2023-12-02 03:44:04 +0530

    updated settings for deployment

--- huddled/settings.py
+++ huddled/settings.py
@@ -17,9 +17,13 @@ load_dotenv(dotenv_path=env_path)
 SECRET_KEY = os.getenv(key='SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.getenv(key='RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
